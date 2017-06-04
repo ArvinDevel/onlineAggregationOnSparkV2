c778a8614763e810b678a2a779eefd1d09f8f6dd
hengyicai <hengyicai@hotmail.com>
2017-06-04 16:09:07 +0800

    Add onlineAvg

--- sql/core/src/main/scala/org/apache/spark/sql/OnlineAggregates.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/OnlineAggregates.scala
@@ -20,23 +20,64 @@ package org.apache.spark.sql
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.types._
 
+import scala.collection.mutable.ListBuffer
+
 class OnlineAvg() extends UserDefinedAggregateFunction {
+
+  // Input Data Type Schema.
+  // Assuming aggregate on single column, and its type is DoubleType.
   override def inputSchema: StructType = {
-    new StructType().add("myinput", DoubleType)
+    new StructType().add("execColumn", DoubleType)
   }
 
+  // Intermediate Schema
   override def bufferSchema: StructType = {
-    new StructType().add("mycnt", LongType).add("mysum", DoubleType)
+    var bufferStrcut = new StructType()
+    bufferStrcut.add("count", LongType)
+    bufferStrcut.add("sum", DoubleType)
+
+    bufferStrcut
   }
 
+  // Return type
   override def dataType: DataType = StringType
-
 
   override def deterministic: Boolean = true
 
+  private val batchSize = 100
+  private var batch = new ListBuffer[Double]()
+  private var batchPivot = 0
+  private var historicalAvg = 0d
+  private var historicalVar = 0d
+  private var crtCount = 0L
+  private var crtSum = 0d
 
+  def updateHistorical(): Unit = {
+    val crtAvg = crtSum / crtCount
+
+    val batchAvg: Double = batch.sum / batch.length
+    val batchVar: Double = calcBatchVar()
+    val historicalCount = crtCount - batch.length
+
+    historicalVar = if (historicalCount == 0) batchVar
+    else (
+      historicalCount * (historicalVar + math.pow(crtAvg - historicalAvg, 2.0)) +
+        batchSize * (batchVar + math.pow(crtAvg - batchAvg, 2.0))
+      ) / (historicalCount + batchSize)
+
+    historicalAvg = if (historicalCount == 0) batchAvg
+    else (crtSum - batch.sum) / (crtCount - batch.length)
+  }
+
+  def calcBatchVar(): Double = {
+    val batchAvg: Double = batch.sum / batch.length
+    batch.foldLeft(0d) { case (sum, sample) =>
+      sum + (sample - batchAvg) * (sample - batchAvg)
+    } / batch.length
+  }
+
+  // Initialize the Intermediate buffer
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
-
     buffer.update(0, 0L)
     buffer.update(1, 0d)
   }
@@ -44,19 +85,35 @@ class OnlineAvg() extends UserDefinedAggregateFunction {
 
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
     buffer.update(0, buffer.getAs[Long](0) + 1)
-
+    crtCount = buffer.getAs[Long](0)
     buffer.update(1, buffer.getAs[Double](1) + input.getAs[Double](0))
+    crtSum = buffer.getAs[Double](1)
+
+    if (batchPivot < batchSize) {
+      batch += input.getAs[Double](0)
+      batchPivot += 1
+    } else {
+      updateHistorical()
+      batch.clear()
+      batchPivot = 0
+    }
   }
 
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
     buffer1.update(0, buffer1.getAs[Long](0) + buffer2.getAs[Long](0))
     buffer1.update(1, buffer1.getAs[Double](1) + buffer2.getAs[Double](1))
-
   }
 
   override def evaluate(buffer: Row): Any = {
     val avg = buffer.getAs[Double](1) / buffer.getAs[Long](0)
-    s"$avg%.2f\tP=0.2\terrorBound=0.01".toString
+
+    updateHistorical()
+
+    val T = math.sqrt(historicalVar)
+    val errorBound: Double = 2.0
+    val confidence = commonMath.calcConfidence(errorBound, crtCount, T)
+
+    s"runningResult=$avg\tP=$confidence\terrorBound=$errorBound".toString
   }
 
 
